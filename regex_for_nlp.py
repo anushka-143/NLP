233866b9cca4e4bc977db81bbb8b4a57fb30a4ca
anushka-143 <106418339+anushka-143@users.noreply.github.com>
2023-07-17 15:16:16 +0530

    Delete regex_for_nlp.py

--- regex_for_nlp.py
+++
@@ -1,8 +0,0 @@
-import re
-
-chat1 = 'Anushka: you ask a lot of questions 1234567890,abc@xyz.com'
-chat2 = 'Anushka: yes phone: (123)-456-7890 , email: abc@xyz.com'
-
-pattern = '\d{10}'
-matches = re.findall(pattern, chat1)
-print(matches)
